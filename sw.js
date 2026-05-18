d400ad711d3a24650a9a2f7b49ed6e4d4d83c44b
Wanghe-1983 <1505301234@qq.com>
2026-05-18 14:15:27 +0800

    fix: 更新CSS/JS版本号和SW缓存名，确保用户加载最新资源

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'indonesian-cache';
+const CACHE_NAME = 'indonesian-cache-v2';
 const ASSETS = ['./','./index.html','./login.html','./admin.html','./indonesian_learning_data.json','./manifest.json','./Wang_he.jpg'];
 
 self.addEventListener('install', e => {
